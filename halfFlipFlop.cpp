525fcd8aad07fecfe9ae438cd1ca1fca15a4046f
Akash Jain <akashjain@Akashs-MacBook-Air.local>
2019-12-20 23:00:58 +0530

    Half Flip Flop

--- halfFlipFlop.cpp
+++ halfFlipFlop.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+// gere c = 0 will return Sum and c = 1 will return Carry
 bool halfAdder(bool X, bool Y, bool c) {
 	bool S,C;
 	S = (X xor Y);
@@ -10,15 +11,11 @@ bool TruthTable() {
 	cout << "X" << "\t" << "Y" << "\t|\t" << "C" << "\t|\t"<< "S"<< endl;
 	// cout << "_" << "_______" << "_" << "_______|_______" << "_" << "_______|_______"<< "_______"<< endl;
 	cout << "_" << "_______" <<"_" << "_______|_______" << "_" << "_______|_______"<< "_______"<< endl;
-
 	for(int i = 0; i < 2; i++) 
 		for (int j = 0; j < 2; ++j) 
 				cout << i << "\t" << j << "\t|\t" << halfAdder(i,j,1) << "\t|\t"<< halfAdder(i,j,0) << endl;
-			
-			
-		
-	
 }
+
 int main() {
 	// inputs
 	bool X = true, Y = true;	
