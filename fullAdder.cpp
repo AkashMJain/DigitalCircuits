d28637d98df9e8e92195e92fa473cfcd4171516f
Akash Jain <akashjain@Akashs-MacBook-Air.local>
2019-12-20 23:04:18 +0530

    full Adder

---
+++ fullAdder.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+bool halfAdder(bool X, bool Y, bool c) {
+	bool S,C;
+	S = (X xor Y);
+	C = X and Y;
+	return (c==0)?S:C;
+}
+/*bool fullAdder(bool x, bool y, bool z) {
+	bool t = halfAdder(x, y)
+}*/
+
+bool fullAdder(bool x, bool y, bool z, bool c) {
+	bool S,C;
+	S = halfAdder(halfAdder(x,y,0),z,0);
+	C = halfAdder(halfAdder(x,y,0),z,1);
+	C = C or halfAdder(x, y, 1);
+	// C = halfAdder(halfAdder(x,y,0),z,1);
+
+
+	return (c==0)?S:C;
+}
+
+bool TruthTable() {
+	cout << "X" << "\t" << "Y" << "\t" << "Z" << "\t|\t" << "C" << "\t|\t"<< "S"<< endl;
+	// cout << "_" << "_______" << "_" << "_______|_______" << "_" << "_______|_______"<< "_______"<< endl;
+	cout << "_" << "_______" << "_" << "_______" <<"_" << "_______|_______" << "_" << "_______|_______"<< "_______"<< endl;
+
+	for(int i = 0; i < 2; i++) 
+		for (int j = 0; j < 2; ++j) 
+			for (int k = 0; k < 2; ++k)
+				cout << i << "\t" << j << "\t" << k <<"\t|\t" << fullAdder(i,j,k,1) << "\t|\t"<< fullAdder(i,j,k,0) << endl;
+			
+			
+		
+	
+}
+int main() {
+	// inputs
+	bool X = true, Y = true;	
+	TruthTable();
+
+
+
+}
